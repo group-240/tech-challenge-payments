34589c59d1f141605c8cc2bd68670656db6e33c8
Lima <andreluiz009@hotmail.com>
2026-01-09 20:34:44 -0300

    fix: downgrade kubernetes provider para 2.20.0 (evita bug Identity Change)

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
+      version = "2.20.0"  # Versão fixa para evitar bug "Unexpected Identity Change"
     }
   }
 
